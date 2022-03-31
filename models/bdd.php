3775c3efbe6759ccefdb02fb3b2cb56e360a9206
khaliloulah2 <79918062+khaliloulah2@users.noreply.github.com>
2022-03-31 17:24:55 +0000

    Update bdd.php

--- models/bdd.php
+++ models/bdd.php
@@ -1,8 +1,8 @@
 <?php
-    $serveur = 'localhost';
-    $nomBd = 'clinique';
-    $user = 'root';
-    $mdp = '';
+    $serveur = 'sql11.freemysqlhosting.net';
+    $nomBd = 'sql11482823';
+    $user = 'sql11482823';
+    $mdp = 'nVYgdmcLYX';
 
     try {
         $bdd = new PDO('mysql:host='.$serveur.';dbname='.$nomBd,$user,$mdp,[PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
